47d31df73285e86009b45f535be2271114a207ba
2002rahuls <rahulhs0212@gmail.com>
2025-09-28 11:27:34 +0530

    Increased the labels andsample sentences

--- ner_lstm/sample_sentences.py
+++ ner_lstm/sample_sentences.py
@@ -1,22 +1,25 @@
-# Example sentences
 sentences = [
-    "Apple is looking at buying U.K. startup for $1 billion",
-    "San Francisco considers banning sidewalk delivery robots",
-    "London is a big city in the United Kingdom"
+    "Peter Parker works at White House",
+    "Sharon Floyd flew to Miami last Friday",
+    "Apple is buying a startup in London",
+    "Many French citizens are going to visit Morocco for summer",
+    "White House announced new policy on Sunday morning"
 ]
 
-# Corresponding NER tags (BIO format)
 labels = [
-    "B-ORG O O O B-LOC O O O O O",
-    "B-LOC I-LOC O O O O O O",
-    "B-LOC O O O O O B-LOC I-LOC"
+    "B-PER I-PER O O B-ORG I-ORG",
+    "B-PER I-PER O O B-LOC O B-TIM I-TIM",
+    "B-ORG O O O O B-LOC",
+    "O B-LOC O O O O B-LOC O O",
+    "B-ORG I-ORG O O O B-TIM I-TIM"
 ]
+
 
 # Tag mapping
 tag_map = {
     "O": 0,
-    "B-ORG": 1,
-    "I-ORG": 2,
-    "B-LOC": 3,
-    "I-LOC": 4
+    "B-PER": 1, "I-PER": 2,
+    "B-ORG": 3, "I-ORG": 4,
+    "B-LOC": 5, "I-LOC": 6,
+    "B-TIM": 7, "I-TIM": 8
 }
